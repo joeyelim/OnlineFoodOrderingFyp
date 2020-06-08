21195493d3de6cfd29885cc7825875e7c67141d6
Kuek Yong Boon <kuekyb@gmail.com>
2020-06-09 01:44:17 +0800

    Implement basic recycleView

--- FYP/app/src/main/java/com/example/fyp/fragments/HomeFragment.kt
+++ FYP/app/src/main/java/com/example/fyp/fragments/HomeFragment.kt
@@ -2,25 +2,25 @@ package com.example.fyp.fragments
 
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.fyp.Class.Canteen
 import com.example.fyp.MainActivity
-
 import com.example.fyp.R
 import com.example.fyp.databinding.FragmentHomeBinding
-import com.example.fyp.databinding.FragmentSignUpBinding
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.fragment_home.*
+import com.google.firebase.firestore.ListenerRegistration
 
 /**
  * A simple [Fragment] subclass.
  */
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var dataListener: ListenerRegistration
 
     companion object {
 
@@ -39,58 +39,76 @@ class HomeFragment : Fragment() {
 
         (activity as MainActivity).setNavVisible()
 
-        binding.textView4.text = "123"
-        initializeFirebase()
+//        binding.textView4.text = "123"
+//        initializeFirebase()
+        initRecycleView()
 
         return binding.root
     }
 
-    private fun initializeFirebase () {
-//        val docRef = FirebaseFirestore.getInstance().collection("Canteen")
-//            .whereEqualTo("type",1)
-//            .get()
+    fun initRecycleView() {
+        val db = FirebaseFirestore.getInstance()
+
+        // Query
+        val query = db.collection(("Canteen"))
+
+        query
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                val adapter = querySnapshot.toObjects(Canteen::class.java)
+                val adapter2 = HomeAdapter()
+                binding.recyclerView.layoutManager = LinearLayoutManager(activity)
+                binding.recyclerView.adapter = adapter2
+                adapter2.data = adapter
+            }
+    }
+
+//    private fun initializeFirebase () {
+////        val docRef = FirebaseFirestore.getInstance().collection("Canteen")
+////            .whereEqualTo("type",1)
+////            .get()
+////
+////        docRef.addOnSuccessListener {
+////            for (document in it) {
+////                Log.d(TAG,"${document.id} => ${document.data}")
+////                binding.textView4.append(document["type"].toString())
+////            }
+////        }  .addOnFailureListener { exception ->
+////            Log.w(TAG, "Error getting documents: ", exception)
+////        }
 //
-//        docRef.addOnSuccessListener {
-//            for (document in it) {
-//                Log.d(TAG,"${document.id} => ${document.data}")
-//                binding.textView4.append(document["type"].toString())
+////        docRef.get().addOnSuccessListener {
+////            Log.w(TAG, "Listen Success.")
+////            binding.textView4.append(it["type"].toString())
+////        }.addOnFailureListener {
+////            Log.w(TAG, "Listen failed.")
+////        }
+//
+//        val docRef = FirebaseFirestore.getInstance().collection("Canteen")
+//        docRef.addSnapshotListener { snapshot, e ->
+//            if (e != null) {
+//                Log.w(TAG, "Listen failed.", e)
+//                return@addSnapshotListener
 //            }
-//        }  .addOnFailureListener { exception ->
-//            Log.w(TAG, "Error getting documents: ", exception)
-//        }
-
-//        docRef.get().addOnSuccessListener {
-//            Log.w(TAG, "Listen Success.")
-//            binding.textView4.append(it["type"].toString())
-//        }.addOnFailureListener {
-//            Log.w(TAG, "Listen failed.")
-//        }
-
-        val docRef = FirebaseFirestore.getInstance().collection("Canteen")
-        docRef.addSnapshotListener { snapshot, e ->
-            if (e != null) {
-                Log.w(TAG, "Listen failed.", e)
-                return@addSnapshotListener
-            }
-
-            if (snapshot != null ) {
-                for (document in snapshot.documents)
-
-                binding.textView4.append(document["type"].toString())
-            } else {
-                Log.d(TAG, "Current data: null")
-            }
-
-
-//            if (snapshot != null && snapshot.exists()) {
-//                Log.d(TAG, "Current data: ${snapshot.data}")
-//                binding.textView4.append(snapshot["type"].toString())
+//
+//            if (snapshot != null ) {
+//                for (document in snapshot.documents)
+//
+//                binding.textView4.append(document["type"].toString())
 //            } else {
 //                Log.d(TAG, "Current data: null")
 //            }
-        }
-
-    }
+//
+//
+////            if (snapshot != null && snapshot.exists()) {
+////                Log.d(TAG, "Current data: ${snapshot.data}")
+////                binding.textView4.append(snapshot["type"].toString())
+////            } else {
+////                Log.d(TAG, "Current data: null")
+////            }
+//        }
+//
+//    }
 
 
 }
