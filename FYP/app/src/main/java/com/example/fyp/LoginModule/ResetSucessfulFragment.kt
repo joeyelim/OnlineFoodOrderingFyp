e7294bb5e99fddaa73eb4f7a90bec0d765ea4ddb
joeyelim <43492692+joeyelim@users.noreply.github.com>
2020-06-10 19:13:09 +0800

    done all login module ui

--- FYP/app/src/main/java/com/example/fyp/LoginModule/ResetSucessfulFragment.kt
+++ FYP/app/src/main/java/com/example/fyp/LoginModule/ResetSucessfulFragment.kt
@@ -11,38 +11,33 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.fyp.MainActivity
 import com.example.fyp.R
-import com.example.fyp.databinding.FragmentForgotPasswordBinding
+import com.example.fyp.databinding.FragmentResetSucessfulBinding
 
 /**
  * A simple [Fragment] subclass.
  */
-class ForgotPasswordFragment : Fragment() {
-    private lateinit var binding: FragmentForgotPasswordBinding
+class ResetSucessfulFragment : Fragment() {
+
+    private lateinit var binding: FragmentResetSucessfulBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_forgot_password, container, false
+        binding= DataBindingUtil.inflate(
+            inflater, R.layout.fragment_reset_sucessful, container, false
         )
         setHasOptionsMenu(true)
         (activity as MainActivity).setNavInvisible()
 
-        binding.btnNext.setOnClickListener{
+        binding.btnLogin.setOnClickListener {
             it.findNavController()
-                .navigate(ForgotPasswordFragmentDirections.actionFragmentForgotPasswordToResetPwdFragment())
-        }
-
-        binding.btnBack.setOnClickListener {
-            it.findNavController()
-                .navigate(ForgotPasswordFragmentDirections.actionForgotPasswordFragmentToFragmentLogin())
-
+                .navigate(ResetSucessfulFragmentDirections.actionResetSucessfulFragmentToFragmentLogin())
         }
 
         return binding.root
     }
+
 
     override fun onPrepareOptionsMenu(menu: Menu) {
         menu.clear()
--- FYP/app/src/main/java/com/example/fyp/OrderingModule/PlaceOrderFragment.kt
+++ FYP/app/src/main/java/com/example/fyp/OrderingModule/PlaceOrderFragment.kt
@@ -1,4 +1,4 @@
-package com.example.fyp
+package com.example.fyp.OrderingModule
 
 
 import android.os.Bundle
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.fyp.R
 
 /**
  * A simple [Fragment] subclass.
