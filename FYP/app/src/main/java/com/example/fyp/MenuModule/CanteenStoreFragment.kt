596836611030811ea5c500c34312f486a6d6c0d4
joeyelim <43492692+joeyelim@users.noreply.github.com>
2020-06-13 03:07:43 +0800

    o

--- FYP/app/src/main/java/com/example/fyp/MenuModule/CanteenStoreFragment.kt
+++ FYP/app/src/main/java/com/example/fyp/MenuModule/CanteenStoreFragment.kt
@@ -3,13 +3,16 @@ package com.example.fyp.MenuModule
 
 import android.os.Bundle
 import android.util.Log
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.Menu
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.fyp.Class.CanteenStore
 import com.example.fyp.FirestoreAdapter.CanteenFireStoreRecyclerAdapter
@@ -31,7 +34,8 @@ class CanteenStoreFragment : Fragment(), onListClick1 {
 
     private lateinit var binding: FragmentCanteenStoreBinding
     private var adapter: StoreFirestoreAdapter? = null
-
+//    private lateinit var navController: NavController
+//    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,6 +50,13 @@ class CanteenStoreFragment : Fragment(), onListClick1 {
         setHasOptionsMenu(true)
         (activity as MainActivity).setNavInvisible()
 
+//        appBarConfiguration = AppBarConfiguration(
+//            topLevelDestinationIds = setOf (
+//                R.id.cartFragment
+//            )
+//
+//        )
+
         initRecycleView()
 
 
@@ -55,6 +66,26 @@ class CanteenStoreFragment : Fragment(), onListClick1 {
     override fun onPrepareOptionsMenu(menu: Menu) {
         menu.clear()
     }
+
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//
+//        val inflater = menuInflater
+//        inflater.inflate(R.menu.food_app_bar, menu)
+//
+//        return true
+//    }
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        when(item.itemId) {
+//            R.id.profileFragment -> {
+//                Toast.makeText(this, "You haven't login...", Toast.LENGTH_LONG).show()
+//                item.onNavDestinationSelected(navController)
+//                return true
+//            }else -> {
+//                return super.onOptionsItemSelected(item)
+//            }
+//        }
+//    }
+//
 
     fun initRecycleView() {
         Log.i("123","123")
@@ -76,7 +107,8 @@ class CanteenStoreFragment : Fragment(), onListClick1 {
 
     override fun onItemClick(store: CanteenStore, position: Int) {
         Log.i("123", store.storeName)
-
+        this.findNavController()
+            .navigate(CanteenStoreFragmentDirections.actionCanteenStoreFragmentToFoodFragment())
 
     }
 
