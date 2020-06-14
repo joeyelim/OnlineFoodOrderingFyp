3fc13ff69930c14f6d98d05ef982bb5ba8a006c4
joeyelim <43492692+joeyelim@users.noreply.github.com>
2020-06-15 00:50:48 +0800

    mini_changer_on_ui

--- FYP/app/src/main/java/com/example/fyp/Class/Food.kt
+++ FYP/app/src/main/java/com/example/fyp/Class/Food.kt
@@ -3,12 +3,12 @@ package com.example.fyp.Class
 class Food {
     var food_name: String? = ""
     var food_image: String? = ""
-    var price: Int? = 0
+    var price: Double? = null
 
     constructor(){
 
     }
-    constructor(food_name: String?, food_image: String?, price : Int?) {
+    constructor(food_name: String?, food_image: String?, price : Double?) {
         this.food_name = food_name
         this.food_image = food_image
         this.price = price
