8b36228a613bf1970d866ce7c122c328ce6d70ad
Kuek Yong Boon <kuekyb@gmail.com>
2020-06-19 20:55:49 +0800

    finish swipe view viewPager

--- FYP/app/src/main/java/com/example/fyp/fragments/OrderListFragment.kt
+++ FYP/app/src/main/java/com/example/fyp/fragments/OrderListFragment.kt
@@ -28,7 +28,7 @@ class OrderListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(
-            inflater, com.example.fyp.R.layout.fragment_order_list, container, false
+            inflater, R.layout.fragment_order_list, container, false
         )
 
         (activity as MainActivity).setNavVisible()
@@ -36,18 +36,18 @@ class OrderListFragment : Fragment() {
 
         /* --------this is for viewpage------------------*/
         
-//        val supportFragmentManager = this.activity!!.supportFragmentManager
-//
-//        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
-//
-//        viewPagerAdapter.setFragment()
-//
-//        val viewPager = binding.viewPager
-//        val tabLayout = binding.tabProgress
-//
-//        viewPager.adapter = viewPagerAdapter
-//        binding.tabProgress.setupWithViewPager(viewPager)
-//        tabLayout.setupWithViewPager(viewPager)
+        val supportFragmentManager = this.activity!!.supportFragmentManager
+
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+
+        viewPagerAdapter.setFragment()
+
+        val viewPager = binding.viewPager
+        val tabLayout = binding.tabProgress
+
+        viewPager.adapter = viewPagerAdapter
+        binding.tabProgress.setupWithViewPager(viewPager)
+        tabLayout.setupWithViewPager(viewPager)
 
 
 
@@ -57,40 +57,40 @@ class OrderListFragment : Fragment() {
     /* --------this is for viewpage------------------*/
 
     // can pass in arrayList of String for maintainability
-//    internal class ViewPagerAdapter(fragmentManager: FragmentManager) :
-//        FragmentPagerAdapter(fragmentManager) {
-//
-//        private val fragments: ArrayList<Fragment> = ArrayList()
-//        private val titles: ArrayList<String> = ArrayList()
-//
-//        override fun getItem(position: Int): Fragment {
-//            //  parameter can use arrayList, probably will
-//            // just use an identifier
-//            return CurrentOrderFragment().newInstance(position.toString())
-//        }
-//
-//        override fun getCount(): Int {
-//            return 4
-//        }
-//
-//        fun addFragment(fragment: Fragment, title: String) {
-//            fragments.add(fragment)
-//            titles.add(title)
-//        }
-//
-//
-//        fun setFragment() {
-//            titles.add("Pending")
-//            titles.add("Preparing")
-//            titles.add("Completed")
-//            titles.add("Taken")
-//        }
-//
-//        override fun getPageTitle(i: Int): CharSequence? {
-//            return titles[i]
-//        }
-//
-//    }
+    internal class ViewPagerAdapter(fragmentManager: FragmentManager) :
+        FragmentPagerAdapter(fragmentManager) {
+
+        private val fragments: ArrayList<Fragment> = ArrayList()
+        private val titles: ArrayList<String> = ArrayList()
+
+        override fun getItem(position: Int): Fragment {
+            //  parameter can use arrayList, probably will
+            // just use an identifier
+            return CurrentOrderFragment().newInstance(titles[position])
+        }
+
+        override fun getCount(): Int {
+            return 4
+        }
+
+        fun addFragment(fragment: Fragment, title: String) {
+            fragments.add(fragment)
+            titles.add(title)
+        }
+
+
+        fun setFragment() {
+            titles.add("Pending")
+            titles.add("Preparing")
+            titles.add("Completed")
+            titles.add("Taken")
+        }
+
+        override fun getPageTitle(i: Int): CharSequence? {
+            return titles[i]
+        }
+
+    }
 
 
 
