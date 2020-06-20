f2ec4818e8eea6d1840e65a37c2b031b130410ee
Kuek Yong Boon <kuekyb@gmail.com>
2020-06-20 09:14:21 +0800

    fix viewpager bug

--- FYP/app/src/main/java/com/example/fyp/fragments/OrderListFragment.kt
+++ FYP/app/src/main/java/com/example/fyp/fragments/OrderListFragment.kt
@@ -11,11 +11,14 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentStatePagerAdapter
 import com.example.fyp.MainActivity
 import com.example.fyp.OrderingModule.CurrentOrderFragment
 import com.example.fyp.OrderingModule.OrderHistoryFragment
-import com.example.fyp.R
 import com.example.fyp.databinding.FragmentOrderListBinding
+
+
+
 
 
 /**
@@ -31,7 +34,7 @@ class OrderListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_order_list, container, false
+            inflater, com.example.fyp.R.layout.fragment_order_list, container, false
         )
 
         (activity as MainActivity).setNavVisible()
@@ -60,16 +63,21 @@ class OrderListFragment : Fragment() {
         binding.tabProgress.setupWithViewPager(viewPager)
         tabLayout.setupWithViewPager(viewPager)
 
-        tabLayout.getTabAt(0)?.setIcon(R.drawable.ic_one)
-        tabLayout.getTabAt(1)?.setIcon(R.drawable.ic_one)
-        tabLayout.getTabAt(2)?.setIcon(R.drawable.ic_one)
+        tabLayout.getTabAt(0)?.setIcon(com.example.fyp.R.drawable.ic_one)
+        tabLayout.getTabAt(1)?.setIcon(com.example.fyp.R.drawable.ic_one)
+        tabLayout.getTabAt(2)?.setIcon(com.example.fyp.R.drawable.ic_one)
+
+//        val badge = tabLayout.getTabAt(0)?.orCreateBadge
+//        badge?.isVisible = true
+//// Optionally show a number.
+//        badge?.number = 99
     }
 
     /* --------this is for viewpage------------------*/
 
     // can pass in arrayList of String for maintainability
     internal class ViewPagerAdapter(fragmentManager: FragmentManager) :
-        FragmentPagerAdapter(fragmentManager) {
+        FragmentStatePagerAdapter(fragmentManager) {
 
         private val fragments: ArrayList<Fragment> = ArrayList()
         private val titles: ArrayList<String> = ArrayList()
