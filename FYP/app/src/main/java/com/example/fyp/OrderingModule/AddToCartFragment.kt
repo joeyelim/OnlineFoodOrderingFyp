a52b4f15869af8d444b5d166a1644887e1a48c1f
joeyelim <43492692+joeyelim@users.noreply.github.com>
2020-06-27 17:20:58 +0800

    done add to cart

--- FYP/app/src/main/java/com/example/fyp/OrderingModule/AddToCartFragment.kt
+++ FYP/app/src/main/java/com/example/fyp/OrderingModule/AddToCartFragment.kt
@@ -1,25 +1,71 @@
 package com.example.fyp.OrderingModule
 
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fyp.FirestoreAdapter.catAdapter
+import com.example.fyp.MainActivity
 import com.example.fyp.R
+import com.example.fyp.ViewModel.CanteenViewModel
+import com.example.fyp.databinding.FragmentAddToCartBinding
+import com.google.firebase.storage.FirebaseStorage
 
 /**
  * A simple [Fragment] subclass.
  */
 class AddToCartFragment : Fragment() {
+    private lateinit var binding: FragmentAddToCartBinding
+    private lateinit var viewModel: CanteenViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_to_cart, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_add_to_cart, container, false
+        )
+
+        viewModel = ViewModelProviders.of(activity!!).get(CanteenViewModel::class.java)
+
+        setHasOptionsMenu(true)
+        (activity as MainActivity).setNavInvisible()
+
+        intiUI()
+
+        return binding.root
     }
 
+
+    private fun intiUI() {
+        binding.txtCanteenName.text = viewModel.canteen.type
+        binding.txtStoreName.text = viewModel.store.store_name
+        binding.txtFoodName.text = viewModel.food.food_name
+        binding.txtFoodName.setPaintFlags( binding.txtFoodName.paintFlags or Paint.UNDERLINE_TEXT_FLAG)
+
+        binding.txtStockQty.text = viewModel.food.total_stock.toString()
+        binding.txtSmallPrice.text = "RM: " + viewModel.food.price.toString()
+        binding.txtLargePrice.text = "RM: " + viewModel.food.price.toString()
+
+        val adapter = catAdapter(viewModel.food.category)
+        binding.rvCat.layoutManager = LinearLayoutManager(activity, RecyclerView.HORIZONTAL, false)
+        binding.rvCat.adapter = adapter
+
+        val image = binding.imgFood
+        val a = FirebaseStorage.getInstance().getReference(viewModel.food.food_image!!)
+        viewModel.setImage(image, a)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        menu.clear()
+    }
 
 }
