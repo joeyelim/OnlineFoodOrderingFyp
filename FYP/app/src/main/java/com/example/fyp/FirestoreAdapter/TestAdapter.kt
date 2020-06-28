6ad21167a54c92ff00aeff0f1c579647d3152c17
joeyelim <43492692+joeyelim@users.noreply.github.com>
2020-06-28 20:12:34 +0800

    1

--- FYP/app/src/main/java/com/example/fyp/FirestoreAdapter/TestAdapter.kt
+++ FYP/app/src/main/java/com/example/fyp/FirestoreAdapter/TestAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.fyp.FirestoreAdapter
 
 import android.graphics.Color
+import android.graphics.Paint
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +9,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fyp.Class.Order_Food
 import com.example.fyp.R
-import com.google.protobuf.Int32Value
 import kotlinx.android.synthetic.main.order_list_row.view.*
 import kotlinx.android.synthetic.main.outer_recycle_view_layout.view.*
 
@@ -34,7 +34,7 @@ class TestAdapter () : RecyclerView.Adapter<TestAdapter.TestViewHolder>() {
     override fun onBindViewHolder(holder: TestViewHolder, position: Int) {
         val food : Order_Food = data[position]
 
-        holder.view.txtCanteenName.text = food.canteen_Name
+        holder.view.txtCanteenName.text = food.food_Name
         holder.view.txtStore.text = food.store_Name
         holder.view.food.text = food.food_Name
         holder.view.txtQuantity.text = food.quantity.toString()
@@ -57,22 +57,24 @@ class TestAdapter () : RecyclerView.Adapter<TestAdapter.TestViewHolder>() {
 
     private fun pendingUI(holder: TestViewHolder) {
         holder.view.btnOrderCancel.visibility = View.VISIBLE
-        holder.view.txtProgress.setTextColor(Color.RED)
+        holder.view.txtProgress.setTextColor(Color.parseColor("#FF7A7B"))
         holder.view.btnOrderCancel.setTextColor(Color.WHITE)
         holder.view.btnOrderCancel.setBackgroundColor(Color.rgb(125,4,4))
     }
 
     private fun preparingUI(holder: TestViewHolder){
         holder.view.btnOrderCancel.visibility = View.GONE
-        holder.view.txtProgress.setTextColor(Color.BLUE)
+        holder.view.txtProgress.setTextColor(Color.parseColor("#034AFF"))
     }
 
     private fun readyUI(holder: TestViewHolder) {
         holder.view.btnOrderCancel.visibility = View.VISIBLE
         holder.view.btnOrderCancel.text = "Paid"
         holder.view.btnOrderCancel.setTextColor(Color.WHITE)
-        holder.view.btnOrderCancel.setBackgroundColor(Color.BLUE)
-        holder.view.txtProgress.setTextColor(Color.rgb(4,200,82))
+        holder.view.btnOrderCancel.setBackgroundColor(Color.parseColor("#034AFF"))
+        holder.view.btnOrderCancel.getPaint().setColor(Color.WHITE)
+        holder.view.btnOrderCancel.getPaint().setStyle(Paint.Style.STROKE)
+        holder.view.txtProgress.setTextColor(Color.parseColor("#04C852"))
     }
 
     private fun takenUI(holder: TestViewHolder) {
--- FYP/app/src/main/java/com/example/fyp/fragments/NotificationFragment.kt
+++ FYP/app/src/main/java/com/example/fyp/fragments/NotificationFragment.kt
@@ -144,21 +144,51 @@ class NotificationFragment : Fragment() {
 
         //--------------------------------------------yien----------------------------------------------------------
 
-//        var food = ArrayList<Food>()
+        var order = ArrayList<Order>()
+
+        order.add(Order("26 June 2020", "26 June 2020", "Dine-In", "Pending",
+            "2:30 PM", "2:30 PM", 9.0, "O100001"))
+
+        order.add(Order("26 June 2020", "26 June 2020", "Take Away", "Pending",
+            "2:40 PM", "2:40 PM", 13.0, "O100002"))
+
+        var cartList = ArrayList<Cart>()
+
+        val date = Calendar.getInstance().time
+        val formatter = SimpleDateFormat("dd.MM.yyyy HH:mm") //or use getDateInstance()
+        val formatedDate = formatter.format(date)
+
+        cartList.add(Cart("Spicy Prawn and Tofu Ramen Noodle Soup", 5.50, 1, "NO.",
+            "Red Bricks Cafeteria", "Noodle", formatedDate ))
+
+//        cartList.add(Cart("Ayam Goreng Berempah", 5.50, 1, "NO.",
+//            "Yum Yum Cafeteria", "Masakan Malaysia", formatedDate ))
 //
-//        food.add(Food("Chicken Rice", "Food/hainanese_chicken_rice.jpg", 4.5, 5.5,
-//            "Nice chicken rice",
-//            2, 3, 20))
-//
+//        cartList.add(Cart("Bihun Goreng", 3.90, 1, "NO.",
+//            "Red Bricks Cafeteria", "Mamak", formatedDate ))
+
+//        cartList.add(Cart("Cafe Americano", 4.80, 2, "NO.",
+//            "Red Bricks Cafeteria", "Cafe", formatedDate ))
 
 
+        var orderfood = ArrayList<Order_Food>()
 
+        orderfood.add(Order_Food("Stewed Sweet and Sour Pork (咕噜肉饭)", 5.5, "Pending", 2,
+            "Add-on tomato source", "Red Bricks Cafeteria", "Mixed Rice", "Dine-In"))
+
+        var orderfood2 = ArrayList<Order_Food>()
+        orderfood2.add(Order_Food("Ristetto Coffee Latte", 6.5, "Pending", 2,
+            "need 3 tsp. sugar & milk", "Red Bricks Cafeteria", "Cafe", "Dine-In"))
 
 
 //        var store = ArrayList<CanteenStore>()
 //
-//        store.add(CanteenStore("Fort Margherita Cafe", "Store/citc_cafe.jpeg",
-//            "Fort Margherita Cafe"))
+//        store.add(CanteenStore("Cafe", "Store/cafe.jpg", "Cafe"))
+//        store.add(CanteenStore("Vegeterian", "Store/vegeterian.png", "Vegeterian"))
+
+
+
+
 
 
 //        var cartList = ArrayList<Cart>()
@@ -203,11 +233,26 @@ class NotificationFragment : Fragment() {
 
 //--------------------------------------------yien----------------------------------------------------------
 
-//        for ((index, item) in food.withIndex()) {
+//        for ((index, item) in order.withIndex()) {
 //            val foodInt = (index)
-//            db.collection("Canteen").document("Yum Yum Cafeteria")
-//                .collection("Store").document("Chicken Rice")
-//                .collection("Food").document(item.food_name!!)
+//            db.collection("User").document("limye-wm18@student.tarc.edu.my")
+//                .collection("Order").document(item.id!!)
+//                .set(item)
+//                .addOnSuccessListener {
+//                    Log.i("upload", "success")
+//                }
+//                .addOnFailureListener {
+//                    Log.i("upload", "Error adding document", it)
+//                }
+//                .addOnCompleteListener {
+//                    Toast.makeText(activity, "Finish Upload Data!", Toast.LENGTH_SHORT).show()
+//                }
+//        }
+//
+//        for ((index, item) in cartList.withIndex()) {
+//            val foodInt = (index)
+//            db.collection("User").document("limye-wm18@student.tarc.edu.my")
+//                .collection("Cart").document(item.cart_ID!!)
 //                .set(item)
 //                .addOnSuccessListener {
 //                    Log.i("upload", "success")
@@ -220,6 +265,40 @@ class NotificationFragment : Fragment() {
 //                }
 //        }
 
+        for ((index, item) in orderfood.withIndex()) {
+            val foodInt = (index)
+            db.collection("User").document("limye-wm18@student.tarc.edu.my")
+                .collection("Order").document("O100001")
+                .collection("Order_Food").document(item.food_Name!!)
+                .set(item)
+                .addOnSuccessListener {
+                    Log.i("upload", "success")
+                }
+                .addOnFailureListener {
+                    Log.i("upload", "Error adding document", it)
+                }
+                .addOnCompleteListener {
+                    Toast.makeText(activity, "Finish Upload Data!", Toast.LENGTH_SHORT).show()
+                }
+        }
+
+
+        for ((index, item) in orderfood2.withIndex()) {
+            val foodInt = (index)
+            db.collection("User").document("limye-wm18@student.tarc.edu.my")
+                .collection("Order").document("O100002")
+                .collection("Order_Food").document(item.food_Name!!)
+                .set(item)
+                .addOnSuccessListener {
+                    Log.i("upload", "success")
+                }
+                .addOnFailureListener {
+                    Log.i("upload", "Error adding document", it)
+                }
+                .addOnCompleteListener {
+                    Toast.makeText(activity, "Finish Upload Data!", Toast.LENGTH_SHORT).show()
+                }
+        }
 
 
     }
