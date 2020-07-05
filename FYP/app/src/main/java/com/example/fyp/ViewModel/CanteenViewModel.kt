fa26af12b15c2d88b12f7fd5ea7b30ae75f68aef
Kuek Yong Boon <kuekyb@gmail.com>
2020-07-05 22:56:35 +0800

    done rating and review

--- FYP/app/src/main/java/com/example/fyp/ViewModel/CanteenViewModel.kt
+++ FYP/app/src/main/java/com/example/fyp/ViewModel/CanteenViewModel.kt
@@ -1,29 +1,66 @@
 package com.example.fyp.ViewModel
 
+import android.util.Log
 import android.widget.ImageView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.fyp.Class.Canteen
 import com.example.fyp.Class.CanteenStore
 import com.example.fyp.Class.Food
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.StorageReference
 import com.squareup.picasso.Picasso
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import com.example.fyp.Class.Notification
 import java.text.DecimalFormat
 
 
 class CanteenViewModel : ViewModel() {
-    var canteen : Canteen = Canteen()
-    var store : CanteenStore = CanteenStore()
-    var food : Food = Food()
+    var canteen: Canteen = Canteen()
+    var store: CanteenStore = CanteenStore()
+    var food: Food = Food()
+    var userRating: Float? = "0.00".toFloat()
+    var newRating : Float? = "0.00".toFloat()
+    var gotReviewed : Boolean = false
 
 
+    private val _rating = MutableLiveData<Float>()
+    val rating: LiveData<Float>
+        get() = _rating
+
+
+    fun setRating(star: Float) {
+        _rating.value = star
+    }
+
+    init {
+        _rating.value = "0.00".toFloat()
+    }
 
     fun setCurrentCanteen(currentCanteen: Canteen) {
         canteen = currentCanteen
     }
 
-    fun setImage(view : ImageView, storage : StorageReference) {
+    fun getUserRating(currentUser: String) {
+        val reviewID = canteen.canteen_name + store.store_name + food.food_name
+        FirebaseFirestore.getInstance()
+            .collection("User").document(currentUser)
+            .collection("User_Review").document(reviewID)
+            .get()
+            .addOnSuccessListener {
+                if (it["star"] == null) {
+                    userRating = "0.00".toFloat()
+                    _rating.value = "0.00".toFloat()
+                    gotReviewed = false
+
+                } else {
+                    userRating = it["star"].toString().toFloat()
+                    _rating.value = it["star"].toString().toFloat()
+                    gotReviewed = true
+                }
+            }
+    }
+
+    fun setImage(view: ImageView, storage: StorageReference) {
         storage.downloadUrl.addOnSuccessListener {
             Picasso.get()
                 .load(it)
@@ -31,7 +68,7 @@ class CanteenViewModel : ViewModel() {
         }
     }
 
-    fun getPrice(price : Double) : String {
+    fun getPrice(price: Double): String {
         val formatter = DecimalFormat("RM ###,###,##0.00")
         return formatter.format(price)
     }
