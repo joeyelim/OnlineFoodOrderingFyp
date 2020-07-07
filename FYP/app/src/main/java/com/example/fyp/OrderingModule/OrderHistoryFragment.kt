0c4febb30d7328667b70028561d9ade48fff43a1
joeyelim <43492692+joeyelim@users.noreply.github.com>
2020-07-07 23:11:14 +0800

    1

--- FYP/app/src/main/java/com/example/fyp/OrderingModule/OrderHistoryFragment.kt
+++ FYP/app/src/main/java/com/example/fyp/OrderingModule/OrderHistoryFragment.kt
@@ -6,14 +6,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.fyp.Class.Order
+import com.example.fyp.Class.Order_Food
+import com.example.fyp.FirestoreAdapter.OrderHistoryFirestoreAdapter
+import com.example.fyp.FirestoreAdapter.onListClick2
 import com.example.fyp.MainActivity
+import com.example.fyp.ViewModel.CartViewModel
+import com.example.fyp.ViewModel.UserViewModel
 import com.example.fyp.databinding.FragmentOrderHistoryBinding
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.FirebaseFirestore
 
 
-class OrderHistoryFragment : Fragment() {
+class OrderHistoryFragment : Fragment(), onListClick2 {
 
     private lateinit var binding: FragmentOrderHistoryBinding
-
+    private lateinit var adapter: OrderHistoryFirestoreAdapter
+    private lateinit var userViewModel: UserViewModel
+    private lateinit var cartViewModel: CartViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,12 +37,71 @@ class OrderHistoryFragment : Fragment() {
         )
 
         setHasOptionsMenu(true)
-        (activity as MainActivity).setNavInvisible()
+        (activity as MainActivity).setNavVisible()
+        userViewModel = ViewModelProviders.of(activity!!).get(UserViewModel::class.java)
+        cartViewModel = ViewModelProviders.of(activity!!).get(CartViewModel::class.java)
 
-
-
+        if (userViewModel.user!!.email != "") {
+            initRecycleView()
+        }
 
         return binding.root
+    }
+
+    private fun initRecycleView() {
+        try {
+            val db = FirebaseFirestore.getInstance()
+            val query = db.collection("User").document(userViewModel.user?.email!!)
+                .collection("Order_Food")
+                .whereEqualTo("status", "Paid")
+
+            query.addSnapshotListener { p0, _ ->
+                if (p0 != null) {
+
+                    if(p0.size() > 0) {
+                        binding.txtOrderHistoryEmpty.visibility = View.GONE
+                        binding.rvOrderHistory.visibility = View.VISIBLE
+                    }
+                    else {
+                        binding.txtOrderHistoryEmpty.visibility = View.VISIBLE
+                        binding.rvOrderHistory.visibility = View.GONE
+                        binding.txtOrderHistoryEmpty.setText("There are currently no order history in here.")
+                    }
+                }
+            }
+
+            val options =
+                FirestoreRecyclerOptions.Builder<Order_Food>()
+                    .setQuery(query, Order_Food::class.java).build()
+
+            adapter =
+                OrderHistoryFirestoreAdapter(options, this, context!!)
+            binding.rvOrderHistory.layoutManager = LinearLayoutManager(context)
+            binding.rvOrderHistory.adapter = adapter
+            binding.rvOrderHistory.isNestedScrollingEnabled = true
+        } catch (e: Exception) {
+
+        }
+    }
+
+//    override fun onItemClick(order: Order, position: Int) {
+//        this.findNavController()
+//            .navigate(OrderHistoryFragmentDirections.actionOrderHistoryFragmentToOrderDetailsFragment())
+//    }
+
+    override fun onStart() {
+        super.onStart()
+        if (userViewModel.user!!.email != "") {
+            adapter.startListening()
+        }
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (userViewModel.user!!.email != "") {
+            adapter.stopListening()
+        }
     }
 
 }
