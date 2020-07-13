a3a7c108c9684a5e1ac17a8dcbbcf45659f846dd
Kuek Yong Boon <kuekyb@gmail.com>
2020-07-13 08:35:25 +0800

    fix profile change password

--- FYP/app/src/main/java/com/example/fyp/stuMaintenanceModule/ChangePwdFragment.kt
+++ FYP/app/src/main/java/com/example/fyp/stuMaintenanceModule/ChangePwdFragment.kt
@@ -55,38 +55,104 @@ class ChangePwdFragment : Fragment() {
         val credential = EmailAuthProvider
             .getCredential(userViewModel.user?.email!!, binding.txtOldPwd.text.toString())
 
-// Prompt the user to re-provide their sign-in credentials
-        user.reauthenticate(credential)
+        val credential2 = EmailAuthProvider
+            .getCredential(userViewModel.user?.email!!, binding.txtNewPwd.text.toString())
+
+        user.reauthenticate(credential2)
             .addOnSuccessListener {
                 Log.i("Test", "Reauth Sucess")
+                binding.txtNewPwdLayout.error = "*New password Cannot Be Same As Old Password"
+                binding.txtNewPwd.requestFocus()
             }
             .addOnFailureListener {
                 Log.i("Test", "Reauth Fail")
+                binding.txtOldPwdLayout.isErrorEnabled = false
+                binding.txtConfirmPwdLayout.isErrorEnabled = false
+                binding.txtNewPwdLayout.isErrorEnabled = false
             }
             .addOnCompleteListener {
-                if (it.isSuccessful) {
-                    user.updatePassword(binding.txtNewPwd.text.toString())
+                if (!it.isSuccessful) {
+                    user.updatePassword(binding.txtOldPwd.text.toString())
                         .addOnSuccessListener {
                             Log.i("Test", "Reset Sucess")
+                            binding.txtOldPwdLayout.isErrorEnabled = false
+                            binding.txtConfirmPwdLayout.isErrorEnabled = false
+                            binding.txtNewPwdLayout.isErrorEnabled = false
                         }
                         .addOnFailureListener {
                             Log.i("Test", "Reset Fail")
+                            binding.txtOldPwdLayout.error = "*Invalid Old Password"
+                            binding.txtOldPwd.requestFocus()
                         }
                         .addOnCompleteListener {
-                            if (it.isSuccessful) {
-                                val snackbar = Snackbar.make(
-                                    this.requireView(), "Password Updated"
-                                    , Snackbar.LENGTH_LONG
-                                )
-                                snackbar.setAction("Close", View.OnClickListener {
-                                    snackbar.dismiss()
-                                })
-                                (snackbar.view).layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
-                                snackbar.show()
-                            }
+                            // Prompt the user to re-provide their sign-in credentials
+                            user.reauthenticate(credential)
+                                .addOnSuccessListener {
+                                    Log.i("Test", "Reauth Sucess")
+                                }
+                                .addOnFailureListener {
+                                    Log.i("Test", "Reauth Fail")
+                                }
+                                .addOnCompleteListener {
+                                    if (it.isSuccessful) {
+                                        user.updatePassword(binding.txtNewPwd.text.toString())
+                                            .addOnSuccessListener {
+                                                Log.i("Test", "Reset Sucess")
+                                            }
+                                            .addOnFailureListener {
+                                                Log.i("Test", "Reset Fail")
+                                            }
+                                            .addOnCompleteListener {
+                                                if (it.isSuccessful) {
+                                                    val snackbar = Snackbar.make(
+                                                        this.requireView(), "Password Updated"
+                                                        , Snackbar.LENGTH_LONG
+                                                    )
+                                                    snackbar.setAction("Close", View.OnClickListener {
+                                                        snackbar.dismiss()
+                                                    })
+                                                    (snackbar.view).layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
+                                                    snackbar.show()
+                                                }
+                                            }
+                                    }
+                                }
                         }
                 }
             }
+
+//// Prompt the user to re-provide their sign-in credentials
+//        user.reauthenticate(credential)
+//            .addOnSuccessListener {
+//                Log.i("Test", "Reauth Sucess")
+//            }
+//            .addOnFailureListener {
+//                Log.i("Test", "Reauth Fail")
+//            }
+//            .addOnCompleteListener {
+//                if (it.isSuccessful) {
+//                    user.updatePassword(binding.txtNewPwd.text.toString())
+//                        .addOnSuccessListener {
+//                            Log.i("Test", "Reset Sucess")
+//                        }
+//                        .addOnFailureListener {
+//                            Log.i("Test", "Reset Fail")
+//                        }
+//                        .addOnCompleteListener {
+//                            if (it.isSuccessful) {
+//                                val snackbar = Snackbar.make(
+//                                    this.requireView(), "Password Updated"
+//                                    , Snackbar.LENGTH_LONG
+//                                )
+//                                snackbar.setAction("Close", View.OnClickListener {
+//                                    snackbar.dismiss()
+//                                })
+//                                (snackbar.view).layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
+//                                snackbar.show()
+//                            }
+//                        }
+//                }
+//            }
     }
 
     private fun inputValidation(): Boolean {
